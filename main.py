4956510097db8f841c654e33148c30d061b09508
MathieuCaro <mathieucaro123@outlook.com>
2025-02-09 16:10:47 +0100

    ajout fenetre basique

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
-from PyQt5.QtGui import QFon
+from PyQt5.QtGui import QFont
+from PyQt5.QtCore import QtAligment
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -9,6 +10,13 @@ class MainWindow(QMainWindow):
         self.setGeometry(700,300,500,500)
         
         label = QLabel("Hello",self)
+        label.setFont(QFont("Arial",30))
+        label.setGeometry(0,0,500,100)
+        label.setStyleSheet("color: blue;"
+                            "background-color:red;"
+                            "font-weight:bold;"
+                            "font-style:italic;")
+        label.setAligment
         
 def main():
     app = QApplication(sys.argv)
